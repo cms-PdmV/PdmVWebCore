abe8c1f91e28e30659129c474098080f1bd8b86b
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2020-11-03 12:37:10 +0100

    Move get_scram_arch to common utils

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,12 +1,21 @@
 """
 Common utils
 """
+import xml.etree.ElementTree as XMLet
+from ..utils.cache import TimeoutCache
+from ..utils.connection_wrapper import ConnectionWrapper
+from ..utils.locker import Locker
+
+# Scram arch cache to save some requests to cmssdt.cern.ch
+__scram_arch_cache = TimeoutCache(3600)
+
 
 def clean_split(string, separator=','):
     """
     Split a string by separator and collect only non-empty values
     """
     return [x.strip() for x in string.split(separator) if x.strip()]
+
 
 def cmssw_setup(cmssw_release):
     """
@@ -21,3 +30,42 @@ def cmssw_setup(cmssw_release):
                 'eval `scram runtime -sh`',
                 'cd ../..']
     return '\n'.join(commands)
+
+
+def get_scram_arch(cmssw_release):
+    """
+    Get scram arch from
+    https://cmssdt.cern.ch/SDT/cgi-bin/ReleasesXML?anytype=1
+    Cache it global variable
+    """
+    if not cmssw_release:
+        return None
+
+    cached_releases = __scram_arch_cache.get('releases', {})
+    cached_value = cached_releases.get(cmssw_release)
+    if cached_value:
+        return cached_value
+
+    with Locker().get_lock('get-scram-arch'):
+        # Maybe cache got updated while waiting for a lock
+        cached_releases = __scram_arch_cache.get('releases', {})
+        cached_value = cached_releases.get(cmssw_release)
+        if cached_value:
+            return cached_value
+
+        connection = ConnectionWrapper(host='cmssdt.cern.ch')
+        response = connection.api('GET', '/SDT/cgi-bin/ReleasesXML?anytype=1')
+        root = XMLet.fromstring(response)
+        releases = {}
+        for architecture in root:
+            if architecture.tag != 'architecture':
+                # This should never happen as children should be <architecture>
+                continue
+
+            scram_arch = architecture.attrib.get('name')
+            for release in architecture:
+                releases[release.attrib.get('label')] = scram_arch
+
+        __scram_arch_cache.set('releases', releases)
+
+    return releases.get(cmssw_release)
