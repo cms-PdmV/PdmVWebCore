4a872c10dfc5dc87fb83c53ff787a359645635ea
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2020-11-20 17:14:08 +0100

    Add option to reuse CMSSW and not cmsrel each time

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -17,18 +17,21 @@ def clean_split(string, separator=','):
     return [x.strip() for x in string.split(separator) if x.strip()]
 
 
-def cmssw_setup(cmssw_release):
+def cmssw_setup(cmssw_release, reuse_cmssw=False):
     """
     Return code needed to set up CMSSW environment for given CMSSW release
     Basically, cmsrel and cmsenv commands
     """
     commands = ['source /cvmfs/cms.cern.ch/cmsset_default.sh',
-                f'if [ -r {cmssw_release}/src ] ; then echo {cmssw_release} already exist',
-                f'else scram p CMSSW {cmssw_release}',
-                'fi',
-                f'cd {cmssw_release}/src',
-                'eval `scram runtime -sh`',
-                'cd ../..']
+                'ORG_PWD=$(pwd)']
+    if reuse_cmssw:
+        commands += ['cd ..']
+
+    commands += [f'if [ ! -r {cmssw_release}/src ] ; then scram p CMSSW {cmssw_release} ; fi',
+                 f'cd {cmssw_release}/src',
+                 'eval `scram runtime -sh`',
+                 'cd $ORG_PWD']
+
     return '\n'.join(commands)
 
 
