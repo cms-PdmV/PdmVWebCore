806545f73ed073c784479e1fe2e9bf2df3167cd0
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2023-07-11 12:10:57 +0200

    Update logger filter
    Append username taken from Flask session cookie to the log

--- utils/username_filter.py
+++ utils/username_filter.py
@@ -3,18 +3,19 @@ This module contans UsernameFilter class
 """
 import logging
 from flask import has_request_context, session
+from ..middlewares.auth import UserInfo
 
 
 class UsernameFilter(logging.Filter):
     """
-    This is a filter that adds Adfs-Login value to the log
+    This is a filter that adds username value to the log
     """
 
     def filter(self, record):
         if has_request_context():
-            user_data = session.get("user")
-            if user_data:
-                record.user = user_data.get("username")
+            user_data: UserInfo | None = session.get("user")
+            if user_data and isinstance(user_data, UserInfo):
+                record.user = user_data.username
             else:
                 record.user = "<anonymous>"
         else:
